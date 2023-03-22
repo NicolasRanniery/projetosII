f8248eb973176b71624365d339acb98902a4cf48
Leonam Bronze <leonambronze02@gmail.com>
2023-03-21 23:07:43 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -4,6 +4,10 @@
 int main()
 {
     printf("Nicolas Ranniery da Silva Sousa - 202206840051\n");
+    printf("Leonam Bronze Moraes Mafra - 202206840057\n")
+    git add atividade_1.c
+    git commit -m "segunda contribuição"
+    git push origin HEAD
  
     return 0;
 }
